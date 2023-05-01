c146481322e85049c3e4ba5987a659fae96c5d67
daniel <danielhakmeh@gmail.com>
2023-05-01 18:44:34 +0300

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,29 @@
+
+import './App.css';
+import {BrouserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from "./Home"
+import About from "./About"
+import index from './index.css'
+
+
+function App() {
+  
+  return (
+    <>
+    <header >
+            <Link to='/' className='site-logo' >#VANLIFE</Link>
+            <nav >
+            <Link to='/about'  >About</Link>
+            </nav>
+        
+    </header>
+   
+    <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={<About/>} />
+      </Routes>
+      </>
+  )
+}
+
+export default App;
